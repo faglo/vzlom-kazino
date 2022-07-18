eb0643742db075704169b95d9591123f384b3f18
Daniil Rakityansky <danch@MBPro.local>
2022-07-19 01:25:40 +0300

    fix

--- взлом казино.py
+++ взлом казино.py
@@ -1,11 +1,14 @@
 from tkinter import *
 from tkinter.ttk import Progressbar, Style
 import webbrowser
+from math import floor
 
 window = Tk()
 window.title("Взлом КАЗИНО")
-Label(window, text="Казино взломано на 14%").pack(side='top')
-Label(window, text="Выкачано 12 050 руб. из 70 055 020 руб.").pack()
+l1 = Label(window, text="Казино взломано на 14%")
+l1.pack(side='top')
+l2 = Label(window, text="Выкачано 12 050 руб. из 70 055 020 руб.")
+l2.pack()
 Label(window, text="Продолжить?").pack()
 style = Style()
 style.theme_use('default')
@@ -18,8 +21,12 @@ bar.pack()
 def on_click():
     for i in range(28, 101):
         bar['value'] = i
+        l1['text'] = "Казино взломано на {}%".format(i)
+        l2['text'] = "Выкачано {} руб. из {} руб.".format(
+            floor(i * 70552020/100), 70552020)
         window.update()
         if i == 100:
+            l1['text'] = "Казино взломано на {}%".format("159 УК РФ")
             webbrowser.open("https://www.youtube.com/watch?v=vTUP8eimDuY")
 
 
